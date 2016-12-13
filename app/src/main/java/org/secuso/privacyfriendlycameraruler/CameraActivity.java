fe69a2d4e634a212edfbac8351a62fe2b6613038
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2016-12-13 02:15:05 +0200

    Added buttons with onClickListeners to CameraActivity.

--- app/src/main/java/org/secuso/privacyfriendlycameraruler/CameraActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlycameraruler/CameraActivity.java
@@ -1,6 +1,8 @@
 package org.secuso.privacyfriendlycameraruler;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by roberts on 12.12.16.
@@ -14,6 +16,23 @@ public class CameraActivity extends BaseActivity {
         setContentView(R.layout.activity_camera);
 
         mSharedPreferences.edit().putString("lastMode", "camera").commit();
+
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.from_camera_button);
+        ImageButton galleryButton = (ImageButton) findViewById(R.id.from_gallery_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         overridePendingTransition(0, 0);
     }
