bbb6d616e8ae10a8470a68f2550cb12e41ad3a5b
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2016-12-13 02:50:45 +0200

    Calls to the camera and the gallery app.

--- app/src/main/java/org/secuso/privacyfriendlycameraruler/CameraActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlycameraruler/CameraActivity.java
@@ -1,6 +1,9 @@
 package org.secuso.privacyfriendlycameraruler;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -9,6 +12,10 @@ import android.widget.ImageButton;
  */
 
 public class CameraActivity extends BaseActivity {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +30,19 @@ public class CameraActivity extends BaseActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, false);
+                startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         });
 
         galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_IMAGE_REQUEST);
             }
         });
 
