f750fe0247897910aceb688a421260ce9753a021
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2016-12-13 03:48:09 +0200

    Now displaying pictures fetched via gallery or camera. Infrastructure for dragging, pinchzooming etc. in place.

--- app/src/main/java/org/secuso/privacyfriendlycameraruler/CameraActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlycameraruler/CameraActivity.java
@@ -1,11 +1,18 @@
 package org.secuso.privacyfriendlycameraruler;
 
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 /**
  * Created by roberts on 12.12.16.
@@ -15,7 +22,27 @@ public class CameraActivity extends BaseActivity {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageButton cameraButton;
+    private ImageButton galleryButton;
+    private ImageView pictureView;
     Uri uri;
+
+    private static final String TAG = "Touch";
+
+    // These matrices will be used to move and zoom image
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+
+    // We can be in one of these 3 states
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    // Remember some things for zooming
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +51,9 @@ public class CameraActivity extends BaseActivity {
 
         mSharedPreferences.edit().putString("lastMode", "camera").commit();
 
-        ImageButton cameraButton = (ImageButton) findViewById(R.id.from_camera_button);
-        ImageButton galleryButton = (ImageButton) findViewById(R.id.from_gallery_button);
+        cameraButton = (ImageButton) findViewById(R.id.from_camera_button);
+        galleryButton = (ImageButton) findViewById(R.id.from_gallery_button);
+        pictureView = (ImageView) findViewById(R.id.pictureView);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +78,33 @@ public class CameraActivity extends BaseActivity {
     }
 
     @Override
-    protected int getNavigationDrawerID() { return R.id.nav_camera; }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE || requestCode == PICK_IMAGE_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                startImageFragment(data.getData());
+            } else {
+                if (resultCode != RESULT_CANCELED) {
+                    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+                        Toast.makeText(this, R.string.camera_crash, Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this, R.string.gallery_crash, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }
+    }
+
+    public void startImageFragment(Uri uri) {
+        cameraButton.setVisibility(GONE);
+        cameraButton.setClickable(false);
+        galleryButton.setVisibility(GONE);
+        galleryButton.setClickable(false);
+        pictureView.setImageURI(uri);
+        pictureView.setVisibility(VISIBLE);
+    }
+
+    @Override
+    protected int getNavigationDrawerID() {
+        return R.id.nav_camera;
+    }
 }
