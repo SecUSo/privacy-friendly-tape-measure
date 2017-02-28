e115077f2d62c07cedb228531cfb4fb70ad746d9
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2017-02-28 23:27:10 +0100

    License text in all classes. Added the license text from README.md to all classes.

--- app/src/main/java/org/secuso/privacyfriendlycameraruler/database/PFASQLiteHelper.java
+++ app/src/main/java/org/secuso/privacyfriendlycameraruler/database/PFASQLiteHelper.java
@@ -1,3 +1,23 @@
+/**
+ * Privacy Friendly Camera Ruler is licensed under the GPLv3. Copyright (C) 2016 Roberts Kolosovs
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ General Public License as published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program.
+ If not, see http://www.gnu.org/licenses/.
+
+ The icons used in the nagivation drawer are licensed under the CC BY 2.5.
+ In addition to them the app uses icons from Google Design Material Icons licensed under Apache
+ License Version 2.0. All other images (the logo of Privacy Friendly Apps, the SECUSO logo and the
+ header in the navigation drawer) copyright Technische Universtität Darmstadt (2016).
+ */
+
 package org.secuso.privacyfriendlycameraruler.database;
 
 import android.content.ContentValues;
