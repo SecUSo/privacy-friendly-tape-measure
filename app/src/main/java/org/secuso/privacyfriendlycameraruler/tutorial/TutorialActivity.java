a91b23336b27928411f7d41d56aef13fae08f15b
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2017-03-08 00:41:53 +0100

    Fixed some layout issues in camera_activity.

--- app/src/main/java/org/secuso/privacyfriendlycameraruler/tutorial/TutorialActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlycameraruler/tutorial/TutorialActivity.java
@@ -31,7 +31,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
