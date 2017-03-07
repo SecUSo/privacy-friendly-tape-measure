6a0c88c6e17882226543c82d58978c4b7d4a8495
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2017-03-07 03:13:35 +0100

    Pictures for the tutorial slides.

--- app/src/main/java/org/secuso/privacyfriendlycameraruler/tutorial/TutorialActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlycameraruler/tutorial/TutorialActivity.java
@@ -1,21 +1,21 @@
 /**
  * Privacy Friendly Camera Ruler is licensed under the GPLv3. Copyright (C) 2016 Roberts Kolosovs
-
- This program is free software: you can redistribute it and/or modify it under the terms of the GNU
- General Public License as published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along with this program.
- If not, see http://www.gnu.org/licenses/.
-
- The icons used in the nagivation drawer are licensed under the CC BY 2.5.
- In addition to them the app uses icons from Google Design Material Icons licensed under Apache
- License Version 2.0. All other images (the logo of Privacy Friendly Apps, the SECUSO logo and the
- header in the navigation drawer) copyright Technische Universtität Darmstadt (2016).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ * <p>
+ * The icons used in the nagivation drawer are licensed under the CC BY 2.5.
+ * In addition to them the app uses icons from Google Design Material Icons licensed under Apache
+ * License Version 2.0. All other images (the logo of Privacy Friendly Apps, the SECUSO logo and the
+ * header in the navigation drawer) copyright Technische Universtität Darmstadt (2016).
  */
 
 package org.secuso.privacyfriendlycameraruler.tutorial;
@@ -93,7 +93,7 @@ public class TutorialActivity extends AppCompatActivity {
         layouts = new int[]{
                 R.layout.tutorial_slide1,
                 R.layout.tutorial_slide2,
-                R.layout.tutorial_slide3,};
+                R.layout.tutorial_slide3};
 
         // adding bottom dots
         addBottomDots(0);
